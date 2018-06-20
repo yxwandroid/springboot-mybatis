fb33a294e41d27a638d508bf58c9ae1cc94c0c51
xuewu.yang <xuewu.yang@leediancn.com>
2018-06-20 17:23:54 +0800

    init

--- src/main/java/com/wilson/nfc/NfcJavaApplication.java
+++ src/main/java/com/wilson/nfc/NfcJavaApplication.java
@@ -1,8 +1,8 @@
 package com.wilson.nfc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan({"com.wilson.nfc.mapper"})
