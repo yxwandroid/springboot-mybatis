0c7301dbe91942b766bb90258b40e15dd7814ac2
xuewu.yang <xuewu.yang@leediancn.com>
2018-06-21 10:38:53 +0800

    add swagger configuration

---
+++ src/main/java/com/wilson/nfc/config/Swagger2.java
@@ -0,0 +1,40 @@
+package com.wilson.nfc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+//                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.wilson.nfc.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("nfc API ")
+                .build();
+//                .description("闻道有先后，术业有专攻。")
+//                .termsOfServiceUrl("http://blog.csdn.net/qq_27093465?viewmode=contents")
+//                .contact(new Contact("csdn大师兄", "http://blog.csdn.net/qq_27093465", "cmshome@163.com"))
+//                .license("")
+//                .licenseUrl("")
+//                .version("1.0.0")
+
+    }
+}
--- src/main/java/com/wilson/nfc/controller/UserController.java
+++ src/main/java/com/wilson/nfc/controller/UserController.java
@@ -2,6 +2,7 @@ package com.wilson.nfc.controller;
 
 import com.wilson.nfc.model.User;
 import com.wilson.nfc.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+
+    @ApiOperation(value = "获取用户信息",notes = "获取登录用户信息")
+    @RequestMapping(value = "getUserInfo", method = RequestMethod.GET)
+    public String getUserInfo() {
+
+        return "userName";
+
+    }
+    @ApiOperation(value = "获取用户信息",notes = "获取登录用户信息")
     @RequestMapping(value = "/api/user/id", method = RequestMethod.GET)
     public User findOneCity(@RequestParam(value = "id", required = true) Integer id) {
         return userService.findUserById(id);
